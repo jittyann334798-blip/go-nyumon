1386022e09f44a304068458fa40bf5376f11b68a
jittyann334798-blip <jittyann334798@gmail.com>
2026-02-27 13:07:28 +0900

    station6

--- app/main.go
+++ app/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	var rootCmd = &cobra.Command{
+		Use:   "dog-cli",
+		Short: "Dog API CLI tool",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("dog-cli 実行成功")
+		},
+	}
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
