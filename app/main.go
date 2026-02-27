16b2b3156d3ceb30ec33fdef0e56f10476fb3f07
jittyann334798-blip <jittyann334798@gmail.com>
2026-02-27 15:38:25 +0900

    station7

--- app/main.go
+++ app/main.go
@@ -1,20 +1,53 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+type DogResponse struct {
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
 func main() {
+
 	var rootCmd = &cobra.Command{
 		Use:   "dog-cli",
-		Short: "Dog API CLI tool",
+		Short: "Dog API CLI",
+	}
+
+	var randomCmd = &cobra.Command{
+		Use:   "random",
+		Short: "ランダムな犬画像を取得",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("dog-cli 実行成功")
+
+			resp, err := http.Get("https://dog.ceo/api/breeds/image/random")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer resp.Body.Close()
+
+			var dog DogResponse
+			if err := json.NewDecoder(resp.Body).Decode(&dog); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Println(dog.Message)
 		},
 	}
+
+	rootCmd.AddCommand(randomCmd)
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
